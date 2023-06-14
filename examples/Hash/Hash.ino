3d9197f541880db6ecffe7edd95a3bde1721c601
as-iotex <as-iotex@users.noreply.github.com>
2023-06-14 13:49:07 +0100

    Improve examples

--- examples/Hash/Hash.ino
+++ examples/Hash/Hash.ino
@@ -1,41 +1,70 @@
-#include "wsiotsdk.h"
+#include "WSIoTSDK.h"
 
-void serial_init(void)
+void setup()
 {
-    Serial.begin(115200);
-    delay(1000);
-    Serial.println();
-}
-
-void sdk_hash_test(void)
-{   
-    psa_status_t status;
-
-    uint8_t sha256_input[7]   = "123456";
-    uint8_t sha256_output[32] = {0};
-    uint8_t sha256_result[32] = {0x8d, 0x96, 0x9e, 0xef, 0x6e, 0xca, 0xd3, 0xc2, 0x9a, 0x3a, 0x62, 0x92, 0x80, 0xe6, 0x86, 0xcf, 0x0c, 0x3f, 0x5d, 0x5a, 0x86, 0xaf, 0xf3, 0xca, 0x12, 0x02, 0x0c, 0x92, 0x3a, 0xdc, 0x6c, 0x92};
-
-    size_t hash_length = 0;
-    
-    Serial.printf("Computing Hash\n");
-    psa_hash_compute( 0x02000009, sha256_input, 6, sha256_output, 32, &hash_length );
-    Serial.printf("Compute Hash finish\n");
-    status = psa_hash_compare( 0x02000009, sha256_input, 6, sha256_result, 32 );
-
-    Serial.printf("Compare Hash result:(0 success, non-zero fail) %d\n", status);
-}
-
-void setup() {
-  
-    serial_init();
-
-    psa_crypto_init(); 
-}
-
-void loop() {
-    
-  sdk_hash_test();
-
+  Serial.begin(115200);
   delay(1000);
+  Serial.println();
 
+  Serial.println("W3bstream SDK example: Hash");
+  Serial.println("This example shows how to use the W3bstream PSA Crypto API compute and validate a hash using a one shot operation.");
+  Serial.println("--------------------------------------------------------------------\n");
+
+  // Initialize the library.
+  psa_crypto_init();
+
+  // Define a variable status that will be used to store the result of the PSA API calls.
+  psa_status_t status = PSA_SUCCESS;
+
+  // The data to hash.
+  uint8_t message[4] = { 0x01, 0x02, 0x03, 0x04 };
+
+  // A buffer where the computed hash will be stored.
+  uint8_t hash[32] = {0};
+
+  // A variable where the length of the computed hash will be stored.
+  size_t hashLenght = 0;
+
+  // Compute the hash using SHA256.
+  status = psa_hash_compute(PSA_ALG_SHA_256, message, sizeof(message), hash, sizeof(hash), &hashLenght);
+
+  // Print the message and the computed hash.
+  Serial.print("Message: ");
+  for (int i = 0; i < sizeof(message); i++)
+  {
+    if (message[i] < 0x10) Serial.print("0");
+    Serial.print(message[i], HEX);
+    Serial.print(" ");
+    if ((i + 1) % 16 == 0) Serial.println();
+  }
+  Serial.println();
+  Serial.print("Hash (");
+  Serial.print(hashLenght);
+  Serial.println(" bytes):");
+  for (int i = 0; i < sizeof(hash); i++)
+  {
+    if (hash[i] < 0x10) Serial.print("0");
+    Serial.print(hash[i], HEX);
+    Serial.print(" ");
+    if ((i + 1) % 16 == 0) Serial.println();
+  }
+  Serial.println();
+
+  // Verify the hash
+  status = psa_hash_compare(PSA_ALG_SHA_256, message, sizeof(message), hash, hashLenght);
+  if (status != PSA_SUCCESS)
+  {
+    Serial.println("Error: psa_hash_compare failed.");
+    Serial.print("Status code: ");
+    Serial.println(status);
+    return;
+  }
+  else
+  {
+    Serial.println("Hash verified successfully.");
+  }
+}
+
+void loop()
+{
 }
