cd9e2b8951ec210ddde3160fe910f6819dda16b4
as-iotex <82106612+as-iotex@users.noreply.github.com>
2023-06-24 15:25:33 +0100

    remove w3bstream related files

--- src/application/devnet/ProtoBuf/protobuf.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PROTOBUF_H__
-#define __PROTOBUF_H__
-
-#include "package.pb.h"
-#include "pb_decode.h"
-#include "pb_encode.h"
-
-#endif
-
--- src/wsiotsdk.h
+++ src/wsiotsdk.h
@@ -3,39 +3,5 @@
 
 #include "include/svc/crypto.h"
 #include "include/entropy.h"
-#include "include/application/devnet/iotex_dev_access.h"
-
-namespace iotex
-{
-class Devnet
-{
-  private:
-
-
-  public:
-
-    void begin(void);
-    void begin(iotex_gettime time_f, iotex_mqtt_pub pub_f, iotex_mqtt_sub sub_f);
-    void setTimeFunction(iotex_gettime time_f);
-    void setMQTTFunctions(iotex_mqtt_pub pub_f, iotex_mqtt_sub sub_f);
-    void setDevnetToken(const char token[], int len);
-    void setDevnetMQTTTopic(const char topic[], int len, int location = 0);
-    void send(void *buf, unsigned int len, enum UserData_Type type);
-
-    char* getMQTTConnectAddr(void);
-    int getMQTTConnectPort(void);    
-
-};
-} // namespace iotex
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-int iotex_wsiotsdk_init(iotex_gettime get_time_func, iotex_mqtt_pub mqtt_pub, iotex_mqtt_sub mqtt_sub);
-#ifdef __cplusplus
-}
-#endif
-
-extern iotex::Devnet devnet_client;
 
 #endif
